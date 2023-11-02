96d23f0328b90d9d866d5ef46e7e8d2fb0d38c6e
Ankit Kansal <ankit.kansal@symbtechnologies.com>
2023-11-02 12:05:24 +0530

    making-UI

---
+++ frontend/src/routes/Routers.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Routes,Route } from 'react-router-dom';
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Services from "../pages/Services";
+import Doctor from "../pages/Doctors/Doctors";
+import DoctorDetails from "../pages/Doctors/DoctorDetails";
+import Signup from "../pages/Signup";
+import Contact from "../pages/Contact";
+
+const Routers = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/home' element={<Home/>}/>
+      <Route path='/doctors' element={<Doctor/>}/>
+      <Route path='/doctors/:id' element={<DoctorDetails/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/register' element={<Signup/>}/>
+      <Route path='/contact' element={<Contact/>}/>
+    </Routes>
+  )
+}
+
+export default Routers
